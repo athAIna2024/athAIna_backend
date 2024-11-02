bf5329b3ae0d0817c0fd82f4c0dc841098c803fe
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-02 13:24:50 +0800

    Changed testresult model studyset_id, learner_id attribute to studyset_instance, learner_instance

--- reportapp/models.py
+++ reportapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class TestResult(models.Model):
-    studyset_id = models.ForeignKey('studysetapp.StudySet', on_delete=models.CASCADE, related_name='testresult')
-    learner_id = models.ForeignKey('accountapp.Learner', on_delete=models.CASCADE, related_name='testresult')
+    studyset_instance = models.ForeignKey('studysetapp.StudySet', on_delete=models.CASCADE, related_name='testresult')
+    learner_instance = models.ForeignKey('accountapp.Learner', on_delete=models.CASCADE, related_name='testresult')
     score = models.IntegerField()
     overall_score = models.IntegerField()
     submitted_at = models.DateTimeField(auto_now_add=True)
