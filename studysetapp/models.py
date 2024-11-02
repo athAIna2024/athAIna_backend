2a3a9ffc7a8eb4f4343c6ce502306046356aa1c2
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-02 12:12:06 +0800

    Updated choices in Studyset Model

--- studysetapp/models.py
+++ studysetapp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class StudySet(models.Model):
-    SUBJECT_CHOICES = [
-        ('ARTS', 'Arts'),
-        ('BUS', 'Business'),
-        ('GEO', 'Geography'),
-        ('ENGR', 'Engineering'),
-        ('HEALTH_MED', 'Health and Medicine'),
-        ('HIST', 'History'),
-        ('LAW_POL', 'Law and Politics'),
-        ('LANG_CULT', 'Languages and Cultures'),
-        ('MATH', 'Mathematics'),
-        ('PHIL', 'Philosophy'),
-        ('SCI', 'Science'),
-        ('SOC_SCI', 'Social Sciences'),
-        ('TECH', 'Technology'),
-        ('WRIT_LIT', 'Writing and Literature')
-    ]
+    class SubjectChoices(models.TextChoices): # Enumerating the choices for the subjects
+        ARTS = 'ARTS', _('Arts')
+        BUSINESS = 'BUS', _('Business')
+        GEOGRAPHY = 'GEO', _('Geography')
+        ENGINEERING = 'ENGR', _('Engineering')
+        HEALTH_MEDICINE = 'HEALTH_MED', _('Health and Medicine')
+        HISTORY = 'HIST', _('History')
+        LAW_POLITICS = 'LAW_POL', _('Law and Politics')
+        LANGUAGES_CULTURES = 'LANG_CULT', _('Languages and Cultures')
+        MATHEMATICS = 'MATH', _('Mathematics')
+        PHILOSOPHY = 'PHIL', _('Philosophy')
+        SCIENCE = 'SCI', _('Science')
+        SOCIAL_SCIENCES = 'SOC_SCI', _('Social Sciences')
+        TECHNOLOGY = 'TECH', _('Technology')
+        WRITING_LITERATURE = 'WRIT_LIT', _('Writing and Literature')
 
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=100)
-    subjects = models.CharField(max_length=20, choices=SUBJECT_CHOICES)
+    subjects = models.CharField(max_length=20, choices=SubjectChoices.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
