a683741718d79a8bf871d9c6d37e25fc6d78b1c7
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-03 18:23:14 +0800

    Created GeneratedTest model for test mode since using serializers is not possible at all.

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class GeneratedTest(models.Model):
+    studyset_instance = models.ForeignKey('studysetapp.StudySet', on_delete=models.CASCADE)
+    learner_instance = models.ForeignKey('accountapp.Learner', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey('flashcardapp.Flashcard', related_name='question_set', on_delete=models.CASCADE)
+    answer = models.ForeignKey('flashcardapp.Flashcard', related_name='answer_set', on_delete=models.CASCADE)
+    learner_answer = models.CharField(max_length=100, blank=True, null=True)
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.studyset_instance} - {self.learner_instance} - {self.created_at}'
