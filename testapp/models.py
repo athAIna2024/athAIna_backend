d5b8169d9a207cbb583dd2cfc3a8e2f7a293b2e1
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-03 19:05:34 +0800

    Remove learner_instance to generatetest model since the studyset_instance is enough to link it to the learner

--- testapp/models.py
+++ testapp/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 class GeneratedTest(models.Model):
     studyset_instance = models.ForeignKey('studysetapp.StudySet', on_delete=models.CASCADE, null=True, blank=True)
-    learner_instance = models.ForeignKey('accountapp.Learner', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     flashcard_instance = models.ForeignKey('flashcardapp.Flashcard', on_delete=models.CASCADE, null=True, blank=True)
     learner_answer = models.CharField(max_length=100, blank=True, null=True)
     correct = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.studyset_instance} - {self.learner_instance} - {self.created_at}'
+        return self.studyset_instance.title + ' - ' + self.flashcard_instance.question
