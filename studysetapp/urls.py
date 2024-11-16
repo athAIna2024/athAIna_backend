22d624f6b7b4c263a137643b7bab416417e1ad8b
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2024-11-16 14:54:00 +0800

    Study Sets Functionalities
    Paginators
    -Create
    -Update
    -Delete
    -Library

--- studysetapp/urls.py
+++ studysetapp/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+
+    path('create/', views.CreateStudySet.as_view(), name='create_studyset'),
+    path('library_studysets/', views.LibraryOfStudySet.as_view(), name='library_StudySet'),
+    path('update/<int:id>/', views.UpdateStudySet.as_view(), name='update_StudySet'),
+
+    # path('delete/<int:id>/', views.DeleteStudySet.as_view(), name='delete_StudySet'),
+
+]
