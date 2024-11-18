9850f60607e69f69dd91f1923e43f83b7d905b0c
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2024-11-18 17:24:34 +0800

    TestApp Update
    - URLS

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('generate_random_flashcards/', views.GenerateRandomFlashcards.as_view(), name='generate_random_flashcards'),
+    path('<uuid:batch_id>/', views.NoBackTracking.as_view(), name='process-generated-tests'),
+    path('<uuid:batch_id>/<int:flashcard_instance>/', views.LearnerAnswerValidation.as_view(),
+         name='validate-learner-answer'),
+    path('<uuid:batch_id>/summary_of_scores/', views.SaveTestResults.as_view(), name='summary-of-scores'),
 ]
