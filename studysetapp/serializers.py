3c73b17bedcb3a6cf162a3158f728630ee4cdcd2
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-11-22 23:18:24 +0800

    Enhance Studyset CRUD operations with learner integration

--- studysetapp/serializers.py
+++ studysetapp/serializers.py
@@ -1,8 +1,49 @@
 from rest_framework import serializers
+
+from accountapp.models import Learner
 from .models import StudySet, Document
 from .validators import validate_file_extension
 
 class StudySetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudySet
+        fields = ['learner_instance', 'title', 'description', 'subjects']
+
+    learner_instance = serializers.PrimaryKeyRelatedField(
+        queryset=Learner.objects.all(),
+        required=True,
+        error_messages={
+            'required': 'Please provide a learner instance',
+        })
+
+    title = serializers.CharField(
+        max_length=100,
+        required=True,
+        allow_blank=False,
+        error_messages={
+            'required': 'Please provide a title',
+            'blank': 'Please provide a title',
+            'max_length': 'Please keep the title under 60 characters.'
+        })
+
+    description = serializers.CharField(
+        max_length=300,
+        required=True,
+        allow_blank=False,
+        error_messages={
+            'required': 'Please provide a description',
+            'blank': 'Please provide a description',
+            'max_length': 'Please keep the description under 100 characters.'
+        })
+
+    subjects = serializers.ChoiceField(
+        choices=StudySet.SubjectChoices.choices,
+        required=True,
+        error_messages={
+            'required': 'Please provide a subject',
+        })
+
+class UpdateStudySetSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudySet
         fields = ['title', 'description', 'subjects']
