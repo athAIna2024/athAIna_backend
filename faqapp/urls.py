6ca509010ada486e6917f699d0951f91e54930d9
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-01-19 18:09:30 +0800

    Remove leading trailing / for faq update delete url

--- faqapp/urls.py
+++ faqapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('save/', views.CreateFAQ.as_view(), name='create_faq'),
-    path('/<int:id>/', views.UpdateAndDeleteFAQ.as_view(), name='update_delete_faq'),
+    path('<int:id>/', views.UpdateAndDeleteFAQ.as_view(), name='update_delete_faq'),
     path('list/', views.ListOfFAQs.as_view(), name='list_faqs'),
 
 ]
