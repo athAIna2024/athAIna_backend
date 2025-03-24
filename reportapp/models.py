f751b45e831df3af802333b2e5f70a86b936634d
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-03-24 14:11:43 +0800

    Refactored GeneratedTest model's attributes by establishing a foreign key for TestBatch instead of just filling the batch_id since it needs to be related for the TestReport model. Also, added an API (POST) endpoint for saving the generated test batch id from frontend to backend.

--- reportapp/models.py
+++ reportapp/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class TestResult(models.Model):
+class TestReport(models.Model):
     studyset_instance = models.ForeignKey('studysetapp.StudySet', on_delete=models.CASCADE, null=True, blank=True)
+    batch = models.ForeignKey('testapp.TestBatch', on_delete=models.CASCADE, null=True, blank=True)
     score = models.IntegerField()
     number_of_questions = models.IntegerField()
-    submitted_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.submitted_at
 
     class Meta:
-        db_table = 'testresults'
+        db_table = 'test_reports'
--- reportapp/urls.py
+++ reportapp/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show-test-result-report/', views.ShowLastSevenTestResults.as_view(), name='show-test-result-report'),
 ]
