1f2210b73a1df60c882842c191bdb741c8ffd99e
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-03-28 19:24:15 +0800

    Added another api method (GET) for fetching list of test scores filtered by subject / studyset_id and date range

--- reportapp/views.py
+++ reportapp/views.py
@@ -7,6 +7,8 @@ from accountapp.models import Learner, User
 from studysetapp.models import StudySet
 from rest_framework.exceptions import NotFound
 from django.http import Http404
+from datetime import datetime
+from django.utils import timezone
 
 class SaveTestScore(generics.CreateAPIView):
     serializer_class = TestReportSerializer
@@ -61,3 +63,46 @@ class ListOfTestScores(generics.ListAPIView):
             'data': serializer.data,
             'successful': True
         }, status=HTTP_200_OK)
+
+class ListOfTestScoresBySubjectAndDate(generics.ListAPIView):
+    serializer_class = TestReportSerializerReadOnly
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('id')
+        studyset_id = self.request.query_params.get('studyset_id')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+
+        if user_id and studyset_id and start_date and end_date:
+            try:
+                user = User.objects.get(id=user_id)
+                learner = Learner.objects.get(user=user)
+                test_scores = TestReport.objects.filter(
+                    studyset_instance_id=studyset_id,
+                    submitted_at__range=[start_date, end_date]
+                )
+
+                return test_scores
+            except Learner.DoesNotExist:
+                raise NotFound({"message": "No learner found with user ID {0}".format(user_id)})
+            except User.DoesNotExist:
+                raise NotFound({"message": "No user found with ID {0}".format(user_id)})
+        return TestReport.objects.none()
+
+    def get(self, request, *args, **kwargs):
+        test_scores = self.get_queryset()
+        serializer = self.get_serializer(test_scores, many=True)
+
+        print(test_scores)
+        if not serializer.data:
+            return Response({
+                'message': 'No test scores found.',
+                'successful': False
+            }, status=HTTP_400_BAD_REQUEST)
+        return Response({
+            'message': 'Test scores found.',
+            'data': serializer.data,
+            'successful': True
+        }, status=HTTP_200_OK)
+
+    
